fca875b5df8c1dcd882f502b2c7b14fcbdb88e37
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2023-11-07 18:48:48 +0100

    added tests for file systems step 1

--- tests/test_file_systems.c
+++ tests/test_file_systems.c
@@ -4,5 +4,12 @@
 
 Test(setting_up_bs, file_systems1, .init = cr_redirect_stdout)
 {
-    cr_assert_eq(fs_open_file("tests.txt"), 0);
+    int res = fs_open_file("../tests/test.txt");
+    cr_assert_stdout_eq_str("SUCCESS\n");
+}
+
+Test(setting_up_bs, file_systems2, .init = cr_redirect_stdout)
+{
+    int res = fs_open_file("test_failure.txt");
+    cr_assert_stdout_eq_str("FAILURE\n");
 }
