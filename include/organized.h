d9d32815c8c2136fcb06d321f252ddfb37c32652
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2023-11-27 12:36:21 +0100

    repo setup

---
+++ include/organized.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2023
+** organized
+** File description:
+** Header for organized
+*/
+
+#ifndef ORGANIZED_H
+    #define ORGANIZED_H
+int organized(void);
+#endif
---
+++ organized.c
@@ -0,0 +1,44 @@
+/*
+** EPITECH PROJECT, 2023
+** organized
+** File description:
+** organized
+*/
+
+#include "include/shell.h"
+
+int add(void *data, char **args)
+{
+    (void) data;
+    (void) args;
+    return 0;
+}
+
+int del(void *data, char **args)
+{
+    (void) data;
+    (void) args;
+    return 0;
+}
+
+int sort(void *data, char **args)
+{
+    (void) data;
+    (void) args;
+    return 0;
+}
+
+int disp(void *data, char **args)
+{
+    (void) data;
+    (void) args;
+    return 0;
+}
+
+int organized(void)
+{
+    int nb = 1;
+    int *pt = &nb;
+
+    return workshop_shell(pt);
+}
