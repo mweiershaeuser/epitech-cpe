c97e286522c085dc544ecf7a136b3b00965f35b7
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2023-12-11 21:41:27 +0100

    feat: added models

--- include/organized.h
+++ include/organized.h
@@ -7,5 +7,30 @@
 
 #ifndef ORGANIZED_H
     #define ORGANIZED_H
+typedef enum type {
+    ACTUATOR,
+    DEVICE,
+    PROCESSOR,
+    SENSOR,
+    WIRE
+} type;
+/*
+static const char *TYPES[] = {
+    "ACTUATOR",
+    "DEVICE",
+    "PROCESSOR",
+    "SENSOR",
+    "WIRE"
+};
+*/
+typedef struct material {
+    int id;
+    type type;
+    char *name;
+} material;
+typedef struct node {
+    void *data;
+    struct node *next;
+} node;
 int organized(void);
 #endif
