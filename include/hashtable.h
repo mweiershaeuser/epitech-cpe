431e63c824fc2df8a2dbb29d77e84f182e240c9a
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2024-01-02 16:19:28 +0100

    feat: modeled hashtable

--- include/hashtable.h
+++ include/hashtable.h
@@ -8,21 +8,26 @@
 #ifndef HASHTABLE_H
     #define HASHTABLE_H
 
+typedef struct entry {
+    int hash;
+    char *value;
+    struct entry *next;
+} entry;
+
 typedef struct hashtable_s {
-    // Your code here
+    int size;
+    int (*hash)(char *, int);
+    entry *table;
 } hashtable_t;
 
-// Hash function
 int hash(char *key, int len);
 
-// Create & destro table
 hashtable_t *new_hashtable(int (*hash)(char *, int), int len);
 void delete_hashtable(hashtable_t *ht);
 
-// Handle table
 int ht_insert(hashtable_t *ht, char *key, char *value);
 int ht_delete(hashtable_t *ht, char *key);
 char *ht_search(hashtable_t *ht, char *key);
 void ht_dump(hashtable_t *ht);
 
-#endif /* HASHTABLE_H */
+#endif
