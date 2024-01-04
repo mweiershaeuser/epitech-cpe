5e79e75f6f1029d834518ae877ba50ea6bbd065c
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2024-01-04 16:37:47 +0100

    feat: added update of existing entries to insert.

--- insert.c
+++ insert.c
@@ -9,10 +9,22 @@
 #include "include/my.h"
 #include "include/hashtable.h"
 
-int ht_insert(hashtable_t *ht, char *key, char *value)
+static entry *find_entry(hashtable_t *ht, int hash, int index)
+{
+    entry *current = ht->table[index];
+
+    while (current != NULL) {
+        if (current->hash == hash)
+            break;
+        current = current->next;
+    }
+    return current;
+}
+
+static entry *create_entry(hashtable_t *ht, char *key, char *value)
 {
     entry *entry;
-    int index = 0;
+    int index;
 
     entry = malloc(sizeof(entry));
     entry->value = malloc(sizeof(char) * my_strlen(value) + 1);
@@ -21,5 +33,22 @@ int ht_insert(hashtable_t *ht, char *key, char *value)
     index = entry->hash % ht->size;
     entry->next = ht->table[index];
     ht->table[index] = entry;
+    return entry;
+}
+
+int ht_insert(hashtable_t *ht, char *key, char *value)
+{
+    entry *entry;
+    int hash;
+    int index;
+
+    hash = ht->hash(key, ht->size);
+    index = hash % ht->size;
+    entry = find_entry(ht, hash, index);
+    if (entry != NULL) {
+        my_strcpy(entry->value, value);
+    } else {
+        entry = create_entry(ht, key, value);
+    }
     return 0;
 }
--- tests/test_insert.c
+++ tests/test_insert.c
@@ -22,3 +22,31 @@ Test(secured, insert_multiple_new_entries)
     cr_assert_str_eq(ht->table[24]->next->value, "tarte");
     delete_hashtable(ht);
 }
+
+Test(secured, insert_update_entry01)
+{
+    hashtable_t *ht = new_hashtable(&hash, 30);
+    ht_insert(ht, "pomme", "compote");
+    ht_insert(ht, "poire", "tarte");
+    ht_insert(ht, "framboise", "macaron");
+    cr_assert_str_eq(ht->table[15]->value, "compote");
+    cr_assert_str_eq(ht->table[24]->value, "macaron");
+    cr_assert_str_eq(ht->table[24]->next->value, "tarte");
+    ht_insert(ht, "framboise", "glace");
+    cr_assert_str_eq(ht->table[24]->value, "glace");
+    delete_hashtable(ht);
+}
+
+Test(secured, insert_update_entry02)
+{
+    hashtable_t *ht = new_hashtable(&hash, 30);
+    ht_insert(ht, "pomme", "compote");
+    ht_insert(ht, "poire", "tarte");
+    ht_insert(ht, "framboise", "macaron");
+    cr_assert_str_eq(ht->table[15]->value, "compote");
+    cr_assert_str_eq(ht->table[24]->value, "macaron");
+    cr_assert_str_eq(ht->table[24]->next->value, "tarte");
+    ht_insert(ht, "poire", "glace");
+    cr_assert_str_eq(ht->table[24]->next->value, "glace");
+    delete_hashtable(ht);
+}
