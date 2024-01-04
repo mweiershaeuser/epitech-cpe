a1609a2258fc59118e1ba205c2ac097c6cbe660e
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2024-01-04 16:04:35 +0100

    feat: added inserting of new entries

--- include/my.h
+++ include/my.h
@@ -12,5 +12,6 @@ void my_put_error(char *msg);
 int my_put_nbr(int nb);
 void my_putchar(char c);
 int my_putstr(char const *str);
+char *my_strcpy(char *dest, char const *src);
 int my_strlen(char const *str);
 #endif
--- insert.c
+++ insert.c
@@ -5,12 +5,21 @@
 ** insert.c
 */
 
+#include <stdlib.h>
+#include "include/my.h"
 #include "include/hashtable.h"
 
 int ht_insert(hashtable_t *ht, char *key, char *value)
 {
-    (void) ht;
-    (void) key;
-    (void) value;
+    entry *entry;
+    int index = 0;
+
+    entry = malloc(sizeof(entry));
+    entry->value = malloc(sizeof(char) * my_strlen(value) + 1);
+    my_strcpy(entry->value, value);
+    entry->hash = ht->hash(key, ht->size);
+    index = entry->hash % ht->size;
+    entry->next = ht->table[index];
+    ht->table[index] = entry;
     return 0;
 }
